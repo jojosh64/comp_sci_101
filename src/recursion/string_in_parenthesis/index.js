8f72e911219b4361703cab5cf737e4506711bf57
Josh Brummund <jojosh64@hotmail.com>
2020-04-30 09:25:37 -0600

    fix: modified code for recursion/string-in-parenthesis to pass failing test

--- src/recursion/string_in_parenthesis/index.js
+++ src/recursion/string_in_parenthesis/index.js
@@ -1,10 +1,12 @@
 export const stringInParenthesis = string => {
+    if (string === '') return string;
+
     if (string[0] === '(' && string[string.length - 1] === ')') return string;
 
-    if (string[string.length - 1] != ")") {
+    if (string[string.length - 1] != ')') {
         return stringInParenthesis(string.substring(0, string.length - 1));
     }
-    if (string[0] != "(") {
+    if (string[0] != '(') {
         return stringInParenthesis(string.substring(1, string.length));
     }
         
